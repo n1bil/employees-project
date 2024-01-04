ab49d2b2c23b7df6fe8ca7e3e436874186ebb1c9
n1bil <nabil.nadzor@gmail.com>
2024-01-04 18:07:10 +0100

    react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Paths } from "./paths";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+
+const router = createBrowserRouter([
+    {
+        path: Paths.home,
+        element: <h1>Employees</h1>,
+    },
+    {
+        path: Paths.login,
+        element: <Login />,
+    },
+    {
+        path: Paths.register,
+        element: <Register />,
+    },
+]);
 
 function App() {
     return (
-      <div>
-        Hello World
-      </div>
+        <RouterProvider router={router}>
+
+        </RouterProvider>
     );
 }
 
