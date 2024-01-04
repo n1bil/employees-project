71dc5794e925216d18b98d81e1e8781bf0546737
n1bil <nabil.nadzor@gmail.com>
2024-01-04 16:02:07 +0100

    Realization methods get, edit, remove

--- backend/src/controller/employees.ts
+++ backend/src/controller/employees.ts
@@ -3,6 +3,21 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+type EmployeeEditRequest = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    age: string;
+    address: string;
+};
+
+type EmployeeCreateRequest = {
+    firstName: string;
+    lastName: string;
+    age: string;
+    address: string;
+};
+
 /**
  * @route GET /api/employees
  * @desc get all employees
@@ -23,7 +38,7 @@ export const getAllEmployees = async (req: Request, res: Response) => {
  * @desc add employee
  * @access Private
  */
-export const addEmployee = async (req: Request, res: Response) => {
+export const addEmployee = async (req: Request<object, object, EmployeeCreateRequest>, res: Response) => {
     try {
         const data = req.body;
 
@@ -34,12 +49,79 @@ export const addEmployee = async (req: Request, res: Response) => {
         const employee = await prisma.employee.create({
             data: {
                 ...data,
-                userId: req.user?.id
+                userId: req.user!.id
             }
         });
 
         return res.status(201).json(employee);
     } catch (error) {
         return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+/**
+ * @route REMOVE /api/employees/remove/:id
+ * @desc remove employee
+ * @access Private
+ */
+export const removeEmployee = async (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        await prisma.employee.delete({
+            where: { id }
+        });
+
+        res.status(200).json({ message: "Employee removed successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Failed to remove employee" });
+    }
+};
+
+/**
+ * @route REMOVE /api/employees/edit/:id
+ * @desc edit employee
+ * @access Private
+ */
+export const editEmployee = async (req: Request<object, object, EmployeeEditRequest>, res: Response) => {
+    const requestEmployee = req.body;
+
+    try {
+        const employee = await prisma.employee.update({
+            where: {
+                id: requestEmployee.id
+            },
+            data: {
+                firstName: requestEmployee.firstName,
+                lastName: requestEmployee.lastName,
+                age: requestEmployee.age,
+                address: requestEmployee.address,
+            }
+        });
+
+        res.status(200).json(employee);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Failed to edit employee" });
+    }
+};
+
+/**
+ * @route GET /api/employees/:id
+ * @desc get employee
+ * @access Private
+ */
+export const getEmployee = async (req: Request<{ id: string }>, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        const employee = await prisma.employee.findFirst({
+            where: { id }
+        });
+
+        res.status(200).json(employee);
+    } catch (error) {
+        return res.status(500).json({ message: "Failed to get employee" });
     }
 };
--- backend/src/routes/employees.ts
+++ backend/src/routes/employees.ts
@@ -1,18 +1,18 @@
 import express from "express";
 import { auth } from "../../middleware/auth";
-import { addEmployee, getAllEmployees } from "../controller/employees";
+import { addEmployee, editEmployee, getAllEmployees, getEmployee, removeEmployee } from "../controller/employees";
 
 const router = express.Router();
 
 // /api/employees
 router.get('/', auth, getAllEmployees);
 // /api/employees/:id
-router.get('/:id', auth, () => console.log('get employee'));
+router.get('/:id', auth, getEmployee);
 // /api/employees/add
 router.post('/add', auth, addEmployee);
 // /api/employees/remove/:id
-router.delete('/remove/:id', auth, () => console.log('remove employee'));
+router.delete('/remove/:id', removeEmployee);
 // /api/employees/edit/:id
-router.put('/remove/:id', auth, () => console.log('edit employee'));
+router.put('/edit', auth, editEmployee);
 
 export default router;
